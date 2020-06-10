87020207702b3da280384896991360d249b440a0
Zev Stravitz <zevstravitz@verizon.net>
2020-06-10 17:37:26 -0400

    full app spec

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -16,5 +16,16 @@ describe ('Test App', () => {
     cy.get('[data-cy=Winter]').click();
     cy.get('[data-cy=course]').should('contain' ,'Winter CS');
   });
+
+  it('Winter is not green before being clicked', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=Winter]').not('have.class', 'is-success');
+  })
+
+  it('changes color when clicked', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=Winter]').click();
+    cy.get('[data-cy=Winter]').should('have.class', 'is-success');
+  })
 });
 
